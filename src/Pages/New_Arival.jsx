ddcbf24d8197c2f23d324d61f39d8aba0e60e08a
sauravknt <sauravknt@gmail.com>
2024-07-05 17:26:38 +0530

    Build commit 1

---
+++ src/Pages/New_Arival.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import New_Arrival from '../Components/New_Arrival/New_Arrival'
+
+const New_Arival = () => {
+  return (
+    <div>
+      <New_Arrival/>
+    </div>
+  )
+}
+
+export default New_Arival
